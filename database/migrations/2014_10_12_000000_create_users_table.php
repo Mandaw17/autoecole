ae1679ea1239b5643c3e2da3b2d7da52fb37faf1
bambademe <cheikhbambademe@gmail.com>
2020-04-14 19:23:18 +0000

    création et initialisation de la base avec la table utilisateur et sa population

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('prenom');
+            $table->string('nom');
+            $table->string('adresse');
+            $table->string('telephone');
+            $table->string('email');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('role_id')->unsigned();
             $table->rememberToken();
             $table->timestamps();
         });
